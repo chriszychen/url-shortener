65783493b1d6a7691f1344c1e6d31a2859eca387
Chris Chen <ok3201123@gmail.com>
2021-05-22 13:16:16 +0800

    feat: add handlebars and modify main.hbs

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
+// variables
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const app = express()
 
+// module setting
 mongoose.connect('mongodb://localhost/url_shortener', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-
 db.on('error', () => {
   console.log('mongodb error!')
 })
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
+// routes setting
 app.get('/', (req, res) => {
   res.send('Hello')
 })
 
+// listening to the server
 app.listen(3000, () => {
   console.log('The server is listening on http://localhost:3000')
 })
